964dfba66dd22db450e24509d3bb905d9451cdb7
Leandro H <ndroher+git@proton.me>
2024-09-28 17:17:19 -0300

    feat: add sign up

--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -3,8 +3,12 @@ import React from "react";
 const types = {
   email: {
     regex:
-      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g,
+      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
     message: "Preencha um e-mail válido",
+  },
+  password: {
+    regex: /^.{5,}$/,
+    message: "A senha precisa ter no mínimo 5 caracteres",
   },
 };
 
@@ -15,7 +19,7 @@ const useForm = (type) => {
   function validate(value) {
     if (type === false) return true;
     if (value.length === 0) {
-      setError("Preencha um valor.");
+      setError("Preencha um valor");
       return false;
     } else if (types[type] && !types[type].regex.test(value)) {
       setError(types[type].message);
