9136a8e00d6eae3914d5dc1c1d6cc40de8c8e57b
Leandro H <ndroher+git@proton.me>
2024-10-03 01:12:50 -0300

    feat(ui): update password-reset pages

--- src/Components/Login/LoginPasswordLost.jsx
+++ src/Components/Login/LoginPasswordLost.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Input from "../Forms/Input";
-import Button from "../Forms/Button";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Waves from "@mui/icons-material/Waves";
 import useForm from "../../Hooks/useForm";
 import useFetch from "../../Hooks/useFetch";
 import { PASSWORD_LOST } from "../../api";
@@ -21,22 +24,87 @@ const LoginPasswordLost = () => {
   }
 
   return (
-    <section>
-      <h1>Esqueceu sua senha?</h1>
-      {data ? (
-        <p>E-mail enviado</p>
-      ) : (
-        <form onSubmit={handleSubmit}>
-          <Input label="E-mail / Usuário" type="text" name="login" {...login} />
-          {loading ? (
-            <Button disabled>Enviando...</Button>
-          ) : (
-            <Button>Enviar</Button>
-          )}
-        </form>
-      )}
-      {error && <p>{error}</p>}
-    </section>
+    <Box
+      component="section"
+      sx={{
+        display: { xs: "grid", md: "block" },
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Box
+        sx={{
+          maxWidth: "30rem",
+          marginTop: "20%",
+          marginLeft: "4rem",
+          marginRight: "4rem",
+          marginBottom: "8rem",
+        }}
+      >
+        <Typography
+          variant="h3"
+          gutterBottom
+          sx={{
+            fontFamily:
+              '"Calistoga", "Roboto", "Helvetica", "Arial", sans-serif;',
+            position: "relative",
+          }}
+        >
+          <Waves
+            color="primary"
+            sx={{
+              position: "absolute",
+              zIndex: "-1",
+              bottom: "5px",
+              left: "-5px",
+            }}
+          />
+          Esqueceu sua senha?
+        </Typography>
+
+        {data ? (
+          <>
+            <Typography variant="subtitle1">
+              E-mail enviado com sucesso!
+            </Typography>
+            <Typography variant="subtitle1">
+              Utilize o link recebido para redefinir sua senha.
+            </Typography>
+          </>
+        ) : (
+          <Box
+            component="form"
+            action=""
+            onSubmit={handleSubmit}
+            sx={{ mb: 3 }}
+          >
+            <Input
+              label="E-mail / Usuário"
+              type="text"
+              name="login"
+              fullWidth
+              sx={{ mb: 2 }}
+              {...login}
+            />
+            {loading ? (
+              <Button fullWidth variant="contained" disabled sx={{ my: 2 }}>
+                Enviando...
+              </Button>
+            ) : (
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ my: 2 }}
+              >
+                Enviar
+              </Button>
+            )}
+            {error && <p>{error}</p>}
+          </Box>
+        )}
+      </Box>
+    </Box>
   );
 };
 
