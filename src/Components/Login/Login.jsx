e380128c9eaf1a8e4d302df087d9dfe9f4789b24
Leandro H <ndroher+git@proton.me>
2024-10-02 23:58:32 -0300

    feat(ui): update login page

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -5,20 +5,50 @@ import LoginCreate from "./LoginCreate";
 import LoginPasswordLost from "./LoginPasswordLost";
 import LoginPasswordReset from "./LoginPasswordReset";
 import { UserContext } from "../../UserContext";
+import Grid from "@mui/material/Grid2";
+import Box from "@mui/material/Box";
+import { useMediaQuery } from "@mui/material";
 
 const Login = () => {
   const { login } = React.useContext(UserContext);
+  const matches = useMediaQuery("(min-width:900px)");
 
   if (login) return <Navigate to="/conta" />;
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="/criar" element={<LoginCreate />} />
-        <Route path="/perdeu" element={<LoginPasswordLost />} />
-        <Route path="/resetar" element={<LoginPasswordReset />} />
-      </Routes>
-    </div>
+    <Grid
+      container
+      spacing={0}
+      sx={{
+        height: `calc(100vh - 64px)`,
+      }}
+    >
+      <Grid
+        size={6}
+        sx={{
+          display: { xs: "none", md: "block" },
+          height: "100%",
+        }}
+      >
+        <Box
+          sx={{
+            backgroundImage:
+              "url('src/Assets/swimmer-with-equipment-pool-full-shot.jpg')",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center center",
+            height: "100%",
+          }}
+        />
+      </Grid>
+      <Grid size={matches ? 6 : 12} sx={{ height: "100%" }}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/criar" element={<LoginCreate />} />
+          <Route path="/perdeu" element={<LoginPasswordLost />} />
+          <Route path="/resetar" element={<LoginPasswordReset />} />
+        </Routes>
+      </Grid>
+    </Grid>
   );
 };
 
--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
+import Link from "@mui/material/Link";
 import Input from "../Forms/Input";
-import Button from "../Forms/Button";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 import useForm from "../../Hooks/useForm";
 import { UserContext } from "../../UserContext";
+import Waves from "@mui/icons-material/Waves";
+import Water from "@mui/icons-material/Water";
 
 const LoginForm = () => {
   const username = useForm();
@@ -18,21 +23,106 @@ const LoginForm = () => {
     }
   }
   return (
-    <section>
-      <h1>Login</h1>
-      <form action="" onSubmit={handleSubmit}>
-        <Input label="Usuário" type="text" name="username" {...username} />
-        <Input label="Senha" type="password" name="password" {...password} />
-        <Link to="/login/perdeu">Esqueceu sua senha?</Link>
-        {loading ? (
-          <Button disabled>Carregando...</Button>
-        ) : (
-          <Button>Entrar</Button>
-        )}
-        {error && <p>{error}</p>}
-      </form>
-      <Link to="/login/criar">Cadastro</Link>
-    </section>
+    <Box
+      component="section"
+      sx={{
+        display: { xs: "grid", md: "block" },
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Box
+        sx={{
+          maxWidth: "30rem",
+          marginTop: "20%",
+          marginLeft: "4rem",
+          marginRight: "4rem",
+          marginBottom: "8rem",
+        }}
+      >
+        <Typography
+          variant="h3"
+          gutterBottom
+          sx={{
+            fontFamily:
+              '"Calistoga", "Roboto", "Helvetica", "Arial", sans-serif;',
+            position: "relative",
+          }}
+        >
+          <Waves
+            color="primary"
+            sx={{
+              position: "absolute",
+              zIndex: "-1",
+              bottom: "5px",
+              left: "-5px",
+            }}
+          />
+          Login
+        </Typography>
+
+        <Box component="form" action="" onSubmit={handleSubmit} sx={{ mb: 3 }}>
+          <Input
+            label="Usuário"
+            type="text"
+            name="username"
+            fullWidth
+            sx={{ mb: 2 }}
+            {...username}
+          />
+          <Input
+            label="Senha"
+            type="password"
+            name="password"
+            fullWidth
+            sx={{ mb: 2 }}
+            {...password}
+          />
+          <Link
+            component={RouterLink}
+            to="/login/perdeu"
+            underline="hover"
+            sx={{ mb: 2 }}
+          >
+            Esqueceu sua senha?
+          </Link>
+          {loading ? (
+            <Button fullWidth variant="contained" disabled sx={{ my: 2 }}>
+              Carregando...
+            </Button>
+          ) : (
+            <Button type="submit" fullWidth variant="contained" sx={{ my: 2 }}>
+              Entrar
+            </Button>
+          )}
+          {error && <p>{error}</p>}
+        </Box>
+
+        <Water
+          color="primary"
+          fontSize="large"
+          sx={{ display: "block", mt: 3, mb: 3 }}
+        />
+        <Box>
+          <Typography
+            variant="h4"
+            gutterBottom
+            sx={{
+              fontFamily:
+                '"Calistoga", "Roboto", "Helvetica", "Arial", sans-serif;',
+            }}
+          >
+            Não possui uma conta?
+          </Typography>
+          <Typography sx={{ mb: 2 }}>
+            Sua jornada aquática começa aqui:{" "}
+            <Link component={RouterLink} to="/login/criar" underline="hover">
+              inscreva-se agora!
+            </Link>
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
