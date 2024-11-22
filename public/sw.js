2e7ac445095b761dda346bce4545b6df88945ed4
Leandro H <ndroher+git@proton.me>
2024-11-22 08:01:08 -0300

    feat(pwa): add background sync

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,7 @@
 import { cleanupOutdatedCaches, precacheAndRoute } from "workbox-precaching";
 import { NavigationRoute, registerRoute } from "workbox-routing";
-import { NetworkFirst } from "workbox-strategies";
+import { NetworkFirst, NetworkOnly } from "workbox-strategies";
+import { BackgroundSyncPlugin } from "workbox-background-sync";
 
 const API_BASE_URL = "http://myswimmingapp.local/json";
 
@@ -57,3 +58,89 @@ const navigationRoute = new NavigationRoute(
   })
 );
 registerRoute(navigationRoute);
+
+// background sync
+const bgSyncPlugin = new BackgroundSyncPlugin("backgroundSyncQueue", {
+  maxRetentionTime: 24 * 60,
+});
+
+// CRIAR EXERCICIO
+registerRoute(
+  ({ url }) => url.href === `${API_BASE_URL}/api/exercicio`,
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  "POST"
+);
+
+// EDITAR EXERCICIO
+registerRoute(
+  ({ url }) => url.pathname.match(/\/api\/exercicio\/\d+/),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  "PUT"
+);
+
+// DELETAR EXERCICIO
+registerRoute(
+  ({ url }) => url.pathname.match(/\/api\/exercicio\/\d+/),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  "DELETE"
+);
+
+// CRIAR TREINO
+registerRoute(
+  ({ url }) => url.href === `${API_BASE_URL}/api/treino`,
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  "POST"
+);
+
+// EDITAR TREINO
+registerRoute(
+  ({ url }) => url.pathname.match(/\/api\/treino\/\d+/),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  "PUT"
+);
+
+// DELETAR TREINO
+registerRoute(
+  ({ url }) => url.pathname.match(/\/api\/treino\/\d+/),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  "DELETE"
+);
+
+// ALTERAR NOME DE EXIBIÇÃO
+registerRoute(
+  ({ url }) => url.href === `${API_BASE_URL}/api/users/me`,
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  "PUT"
+);
+
+// ALTERAR FOTO DE PERFIL
+registerRoute(
+  ({ url }) => url.href === `${API_BASE_URL}/api/conta/foto`,
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  "POST"
+);
+
+// ALTERAR METAS
+registerRoute(
+  ({ url }) => url.href === `${API_BASE_URL}/api/user/goals`,
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  "PUT"
+);
